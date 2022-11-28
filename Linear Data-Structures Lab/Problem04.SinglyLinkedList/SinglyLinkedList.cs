26ef3f0ea7206c0352a0f3c374b9dd0b924d3568
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-11-29 00:01:49 +0200

    Implemented Singly linked list

--- Linear Data-Structures Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ Linear Data-Structures Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,133 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private class Node
+        {
+            public Node(T value, Node next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+            public T Value { get; set; }
+            public Node Next { get; set; }
+        }
+        private Node head;
+
+        public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                var newHead = new Node(item, null);
+                this.head = newHead;
+            }
+            else
+            {
+                var oldHead = this.head;
+                var newHead = new Node(item, oldHead);
+                this.head = newHead;
+            }
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
-        }
+            var newNode = new Node(item, null);
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var currentNode = this.head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = newNode;
+            }
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.CheckCollectionSize();
+            return this.head.Value;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.CheckCollectionSize();
+
+            var currentNode = this.head;
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+            return currentNode.Value;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.CheckCollectionSize();
+            var oldHead = this.head;
+            var newHead = this.head.Next;
+
+            this.head = newHead;
+            this.Count--;
+            return oldHead.Value;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.CheckCollectionSize();
+            T removedValue = default;
+
+            if (this.head.Next == null)
+            {
+                removedValue = this.head.Value;
+                this.head = null;
+                this.Count--;
+                return removedValue;
+            }
+
+            var currentNode = this.head;
+            //Check if current node is second to last
+            while (currentNode.Next.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+            //Save removed node value
+            removedValue = currentNode.Next.Value;
+            //Remove last node by setting it to null
+            currentNode.Next = null;
+            this.Count--;
+            //Return saved value
+            return removedValue;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void CheckCollectionSize()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot perform this operation on an empty collection");
+            }
         }
     }
 }
