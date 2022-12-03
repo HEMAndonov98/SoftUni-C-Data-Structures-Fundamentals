65b5a6e121a007f2e5995e0756e5963b7ac9e165
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-12-03 13:21:39 +0200

    Doubly linked List

--- Linear-Data-Structures Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Linear-Data-Structures Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -6,46 +6,149 @@
 
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
+        private class Node
+        {
+            public Node(T value, Node next, Node previous)
+            {
+                this.Value = value;
+                this.Next = next;
+                this.Previous = previous;
+            }
+
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+            public T Value { get; set; }
+        }
+
+        private Node head;
+        private Node tail;
+
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                this.AddToEmptyCollection(item);
+            }
+            else
+            {
+                var oldHead = this.head;
+                var newHead = new Node(item, oldHead, null);
+
+                oldHead.Previous = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                this.AddToEmptyCollection(item);
+            }
+            else
+            {
+                var oldTail = this.tail;
+                var newTail = new Node(item, null, oldTail);
+
+                oldTail.Next = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.CheckIfListEmpty();
+            return this.head.Value;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.CheckIfListEmpty();
+            return this.tail.Value;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.CheckIfListEmpty();
+
+            var oldHead = this.head;
+
+            if (this.Count == 1)
+            {
+                this.ClearCollection();
+            }
+            else
+            {
+                var newHead = this.head.Next;
+                newHead.Previous = null;
+
+                this.head = newHead;
+            }
+
+            this.Count--;
+            return oldHead.Value;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.CheckIfListEmpty();
+
+            var oldTail = this.tail;
+
+            if (this.Count == 1)
+            {
+                this.ClearCollection();
+            }
+            else
+            {
+                var newTail = this.tail.Previous;
+                newTail.Next = null;
+
+                this.tail = newTail;
+            }
+
+            this.Count--;
+            return oldTail.Value;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void CheckIfListEmpty()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void AddToEmptyCollection(T item)
+        {
+            var newNode = new Node(item, null, null);
+            this.head = newNode;
+            this.tail = newNode;
+        }
+
+        private void ClearCollection()
+        {
+            this.head = null;
+            this.tail = null;
         }
     }
 }
