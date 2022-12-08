08d13c58ed51804559d6d430e803c7f177f79a0e
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-12-08 13:32:05 +0200

    Finished balanced parentheses problem

--- Linear-Data-Structures Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Linear-Data-Structures Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,45 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            var stack = new Stack<char>(parentheses.Length / 2);
+
+            if (parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            foreach (var singleParenthese in parentheses)
+            {
+                char expectedParenthese = default;
+
+                switch (singleParenthese)
+                {
+                    case ')':
+                        expectedParenthese = '(';
+                        break;
+                    case '}':
+                        expectedParenthese = '{';
+                        break;
+                    case ']':
+                        expectedParenthese = '[';
+                        break;
+                    default:
+                        stack.Push(singleParenthese);
+                        break;
+                }
+
+                if (expectedParenthese == default)
+                {
+                    continue;
+                }
+
+                if (stack.Pop() != expectedParenthese)
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
         }
     }
 }
