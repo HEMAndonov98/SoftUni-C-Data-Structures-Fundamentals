780d282c8d607b7ce74c4f8f7dafc0340de30305
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2023-01-02 17:16:28 +0200

    Two implementations of Contains

--- Heaps, BST/06.Heaps-BST-Lab-Skeleton/02.BinarySearchTree/BinarySearchTree.cs
+++ Heaps, BST/06.Heaps-BST-Lab-Skeleton/02.BinarySearchTree/BinarySearchTree.cs
@@ -25,8 +25,46 @@
         }
 
         public bool Contains(T element)
+            //=> this.Contains(element, this.root) != null;
+            => this.Contains2(element, this.root); //(this method I implemented uses recursion to find the node)//
+
+        private Node Contains(T element, Node node)
         {
-            throw new NotImplementedException();
+            while (node != null)
+            {
+                if (element.CompareTo(node.Value) < 0)
+                {
+                    node = node.LeftChild;
+                }
+                else if (element.CompareTo(node.Value) > 0)
+                {
+                    node = node.RightChild;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return node;
+        }
+        private bool Contains2(T element, Node node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+               return this.Contains2(element, node.LeftChild);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                return this.Contains2(element, node.RightChild);
+            }
+
+            return true;
         }
 
         public void EachInOrder(Action<T> action)
@@ -39,20 +77,22 @@
             this.root = this.Insert(element, this.root);
         }
 
-        private Node Insert(T element, Node root)
+        private Node Insert(T element, Node node)
         {
-            if (root == null)
+            if (node == null)
             {
-                root = new Node(element);
+                node = new Node(element);
             }
-            else if (element.CompareTo(root.Value) < 0)
+            else if (element.CompareTo(node.Value) < 0)
             {
-               root.LeftChild = this.Insert(element, root.LeftChild);
+               node.LeftChild = this.Insert(element, node.LeftChild);
             }
-            else if (element.CompareTo(root.Value) > 0)
+            else if (element.CompareTo(node.Value) > 0)
             {
-                root.RightChild = this.Insert(element, root.RightChild);
+                node.RightChild = this.Insert(element, node.RightChild);
             }
+
+            return node;
         }
 
         public IBinarySearchTree<T> Search(T element)
